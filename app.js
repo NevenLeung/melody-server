3428fd329219abeb8855057f0f4d3dd75b7a74a2
Neven Leung <nevyn.leong@gmail.com>
2017-09-30 14:33:17 +0800

    connect to mongodb

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var config = require('./config');
+
+mongoose.connect(config.mongoDBUrl, {useMongoClient: true});
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Connection error'));
+db.once('open', function () {
+    console.log('Connected successfully to server');
+});
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -27,20 +38,20 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
